18dcd9a59682b308cb2b0175cbbd308353ad5abd
Juan Espinosa <juancamiloespinosab@gmail.com>
2023-03-08 17:28:47 -0500

    core: Agregar modulos para las vistar principales: "Auth" y "Subscribers"

---
+++ src/app/modules/subscribers/subscribers.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SubscribersRoutingModule } from './subscribers-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SubscribersRoutingModule
+  ]
+})
+export class SubscribersModule { }
