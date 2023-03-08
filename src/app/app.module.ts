b4d8512eff6ae854a517c4c65929afecce44db5e
Juan Espinosa <juancamiloespinosab@gmail.com>
2023-03-08 17:02:08 -0500

    core: Instalar libreria "@auth0/angular-jwt"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { JwtModule } from '@auth0/angular-jwt';
+
+import { AppComponent } from './app.component';
+
+function tokenGetter() {
+  return localStorage.getItem('access_token');
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +21,9 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
+    JwtModule.forRoot({
+      config: { tokenGetter: tokenGetter, allowedDomains: ['localhost:3000'] },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
