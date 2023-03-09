259d9df650e53aaf1635216e323db1b588765250
Juan Espinosa <juancamiloespinosab@gmail.com>
2023-03-09 16:56:18 -0500

    fix(logo): Ajustar color de logo en el navbar

--- src/app/components/atomic-components/organisms/navbar/navbar.component.ts
+++ src/app/components/atomic-components/organisms/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LogoComponent } from '@components/atomic-components';
+import { LOGO_TYPE } from '@components/enums';
 import { SharedModule } from '@shared/shared.module';
 
 @Component({
@@ -10,6 +11,8 @@ import { SharedModule } from '@shared/shared.module';
   standalone: true,
 })
 export class NavbarComponent implements OnInit {
+  logoType: LOGO_TYPE = LOGO_TYPE.WHITE
+
   constructor() {}
 
   ngOnInit(): void {}
