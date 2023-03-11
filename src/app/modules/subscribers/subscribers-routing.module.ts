f7ff8180d193d70519a088b466c772be7c38eab2
Juan Espinosa <juancamiloespinosab@gmail.com>
2023-03-11 01:41:11 -0500

    feat: Agregar guard a las rutas internas de /subscribers

--- src/app/modules/subscribers/subscribers-routing.module.ts
+++ src/app/modules/subscribers/subscribers-routing.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@core/guards/auth.guard';
 import { CreateEditComponent } from './pages/create-edit/create-edit.component';
 import { ListComponent } from './pages/list/list.component';
 
 const routes: Routes = [
   {
     path: 'list',
+    canActivate: [AuthGuard],
     component: ListComponent,
   },
   {
     path: 'create',
+    canActivate: [AuthGuard],
     data: { editMode: false },
     component: CreateEditComponent,
   },
   {
     path: 'edit/:id',
+    canActivate: [AuthGuard],
     data: { editMode: true },
     component: CreateEditComponent,
   },
