0e45dc76af9b3d712209c33bc8b629126d57e884
Juan Espinosa <juancamiloespinosab@gmail.com>
2023-03-10 22:40:10 -0500

    feat: Agregar configuraciones de Firebase para realizar despliegue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   invertebradoApi: {
+    domain: 'lab.app.invertebrado.co',
     baseUrl: 'https://lab.app.invertebrado.co/api',
     paths: {
       account: {
